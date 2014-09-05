9a3ef4fa211a7069a7542d00a49115889c6eb641
sarapereira <smp217@lehigh.edu>
2014-09-05 14:39:53 +0000

    updated HelloWorld

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,26 @@
+// Welcome Class java program
+//
+
+// define a class
+public class WelcomeClass{
+    
+// add main method
+    public static void main(String[ ] args) {
+
+// print welcome message with Lehigh user ID and autobiography
+
+System.out.println("    -----------     ");
+System.out.println("    | WELCOME |     ");
+System.out.println("    -----------     ");
+System.out.println("  ^  ^  ^  ^  ^  ^   ");
+System.out.println("                     ");
+System.out.println("|-S--M--P--2--1--7-| ");
+System.out.println("                     ");
+System.out.println("  v  v  v  v  v  v   ");
+
+System.out.println("My name is Sara and I love baking, shopping, and traveling.");
+System.out.println("I'm excited to be starting my sophomore year!");
+
+//test/testtest
+    }
+}
